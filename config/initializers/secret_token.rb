1488d14253ffa2cbe813c374835c2b685297aa53
Aliz Karami <aliz.karami@gmail.com>
2013-06-17 10:20:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuApp::Application.config.secret_token = '76231706ff6719c3f753b75df97d02ee76fabfab7b35c1c9a6ac582855b2f5ccbac6544a2b33bc6cdbb7181195875cdbe197fd8bb46d7855d8334f741b15bf0f'
---
+++ web.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+
+get '/' do
+  "Hello, This is Aliz!"
+end
